13bc90f2a71e536dd88a1b95a19fe46143edf549
Helena Gonzalez <helenagonzalez@iMac-de-Helena.local>
2025-04-11 13:41:31 +0200

    login file added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Base from './pages/bases/base';
+import Login from './pages/login/login';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path='/login' element={<Login />} />
           <Route path='/' element={<Base />} />
         </Routes>
       </BrowserRouter>
