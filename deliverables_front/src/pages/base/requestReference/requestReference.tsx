ff76c64dc083d0bf61cd2037daf9ac2cdf6dd4ac
Helena Gonzalez <helenagonzalez@iMac-de-Helena.local>
2025-04-15 11:30:53 +0200

    request reference page added

--- deliverables_front/src/pages/base/requestReference/requestReference.tsx
+++ deliverables_front/src/pages/base/requestReference/requestReference.tsx
@@ -1,7 +1,25 @@
+import { Header, Input } from '@airbus/components-react';
+import { useTranslation } from 'react-i18next';
+import './requestReference.css';
+
+//TODO: input onChange
+
 const RequestReference = () => {
-    return (
-      <div>RequestReference</div>
-    )
-  }
-  
-  export default RequestReference
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <Header title={t('common:title')} appName={t('common:title')} />
+      <div className='referenceContainer'>
+        <div className='referenceInput'>
+          <Input
+            placeholder={t('common:referenceInput')}
+            // onChange={}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default RequestReference;
